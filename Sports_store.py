fb609ff7bba1ca6d5d5be045abe0ad512c896878
leonardo-200224 <ayalaperezleonardo24@gmail.com>
2026-03-19 08:37:07 -0500

    Sports_store

--- Sports_store.py
+++ Sports_store.py
@@ -9,6 +9,7 @@ Practica: ciclo, contador, condicional.
 products = []
 num=100000
 counter=0
+
 for cont in range(1,7):
     product =input(f"enter product {cont}: ").strip()
     price = float(input(f"enter price of {product}: "))
@@ -24,5 +25,15 @@ for cont in range(1,7):
     
 
 for cont1 in products:
-    print("product": ,cont1["product"],"\n",
-          "price": ,cont1["price"])
+    print(f"product: {cont1["product"]} \n"
+        f"price: ${cont1["price"]:,.0f}\n"
+        )
+print(f"greater than 100.000: {counter}")
+
+# {valor:,.0f} → Formatea un número de la siguiente manera:
+# valor → es la variable que contiene el número
+# :     → indica que se va a aplicar un formato
+# ,     → agrega separador de miles (ej: 200000 → 200,000)
+# .0    → indica que no se mostrarán decimales
+# f     → indica que el número es tipo float
+# el valor se REDONDEA automáticamente (ej: 200000.75 → 200,001)
